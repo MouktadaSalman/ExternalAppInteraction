778efd46b410231def6b47ce72b6f85b831c5b8e
Mouktada Salman <20640266@student.curtin.edu.au>
2024-09-20 14:59:47 +0800

    email and phone

--- app/src/main/java/com/example/externalappinteraction/PickContact.java
+++ app/src/main/java/com/example/externalappinteraction/PickContact.java
@@ -9,13 +9,19 @@ import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PickContact extends AppCompatActivity {
-    private long contactId;
+    int contactId;
+    Button pickContact;
+    Button more;
+    TextView phone;
+    TextView email;
+    TextView name;
 
     ActivityResultLauncher<Intent> pickContactLauncher =
             registerForActivityResult(
@@ -30,6 +36,12 @@ public class PickContact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        name = findViewById(R.id.nameView);
+        email = findViewById(R.id.emailView);
+        phone = findViewById(R.id.phoneView);
+        more = findViewById(R.id.moreButton);
+        pickContact = findViewById(R.id.pickContact);
+
 
     }
 
@@ -38,6 +50,68 @@ public class PickContact extends AppCompatActivity {
         intent.setAction(Intent.ACTION_PICK);
         intent.setData(ContactsContract.Contacts.CONTENT_URI);
         pickContactLauncher.launch(intent);
+    }
+
+    private void morButtonClicked(){
+        String result = "";
+        Uri emailUri = ContactsContract.CommonDataKinds.Email.CONTENT_URI;
+        String[] queryFields = new String[]{
+                ContactsContract.CommonDataKinds.Email.ADDRESS
+        };
+
+        String whereClause = ContactsContract.CommonDataKinds.Email.CONTACT_ID + "=?";
+        String [] whereValues = new String[]{
+          String.valueOf(this.contactId)
+        };
+
+        Cursor cursor = getContentResolver().query(
+                emailUri, queryFields, whereClause, whereValues, null);
+
+        try{
+            cursor.moveToFirst();
+            do{
+                String emailAddress = cursor.getString(0);
+                result = result + emailAddress + " ";
+            }while (cursor.moveToNext());
+        }
+        finally {
+            cursor.close();
+        }
+
+        email.setText(result);
+        email.setVisibility(View.VISIBLE);
+    }
+
+    private void getPhoneNumber(){
+        String result = "";
+        Uri phoneUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String[] queryFields = new String[]{
+          ContactsContract.CommonDataKinds.Phone.NUMBER
+        };
+
+        String whereClause = ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=?";
+        String[] whereValue = new String[]{
+                String.valueOf(this.contactId)
+        };
+
+        Cursor cursor = getContentResolver().query(
+                phoneUri, queryFields, whereClause, whereValue, null
+        );
+
+        try{
+            cursor.moveToFirst();
+
+            do{
+                String phoneNumber = cursor.getString(0);
+                result = result + phoneNumber + " ";
+            }while (cursor.moveToNext());
+        }
+        finally {
+            cursor.close();
+        }
+
+        phone.setText(result);
+        phone.setVisibility(View.VISIBLE);
     }
 
     private void processPickContractResult(Intent data){
@@ -53,8 +127,9 @@ public class PickContact extends AppCompatActivity {
                 cursor.moveToFirst();
                 this.contactId = cursor.getInt(0);
                 String contactName = cursor.getString(1);
-
-
+                name.setVisibility(View.VISIBLE);
+                name.setText(contactName);
+                more.setVisibility(View.VISIBLE);
             }
         }
         finally {
